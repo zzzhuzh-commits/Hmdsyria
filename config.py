f61e1bb28b533d615672bb5ee43610b54ebd14f5
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-21 11:01:56 +0200

    Initialize config.py with environment variables

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH")
+MONGO_URL = os.getenv("MONGO_URL")
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
