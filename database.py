f0b3312d1509c4ca7a247fef9ce2898828fac97e
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-21 11:06:46 +0200

    Change database name from vc_music_bot to vc_ultra_bot

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ from pymongo import MongoClient
 import config
 
 client = MongoClient(config.MONGO_URL)
-db = client["vc_music_bot"]
+db = client["vc_ultra_bot"]
 
 queue = db["queue"]
+state = db["state"]
+sudo = db["sudo"]
